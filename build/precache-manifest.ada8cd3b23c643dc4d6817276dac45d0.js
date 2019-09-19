066040901d067e770d3fafd56aaaa9a842f88820
lwg <18713571387@163.com>
2019-09-20 02:13:14 +0800

    second update

---
+++ build/precache-manifest.ada8cd3b23c643dc4d6817276dac45d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5db9afbf7fc04ac03980a37280a2e61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "597a5e3a8aef12fea930",
+    "url": "./static/css/main.7ca6bae0.chunk.css"
+  },
+  {
+    "revision": "ee3dcd2210ccffe4b8a9",
+    "url": "./static/js/2.296b1210.chunk.js"
+  },
+  {
+    "revision": "597a5e3a8aef12fea930",
+    "url": "./static/js/main.a9bcf8b5.chunk.js"
+  },
+  {
+    "revision": "e6cd9f5791da69105218",
+    "url": "./static/js/runtime~main.640b191e.js"
+  }
+]);
